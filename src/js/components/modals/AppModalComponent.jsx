a4fc8a47f2ccec78168d986e22f107c7eee7e7f1
Felix Gertz <felix@mesosphere.io>
2015-08-07 13:00:53 +0200

    Moving AppModalComponent to modals directory

--- src/js/components/modals/AppModalComponent.jsx
+++ src/js/components/modals/AppModalComponent.jsx
@@ -1,22 +1,22 @@
 var _ = require("underscore");
 var lazy = require("lazy.js");
 var React = require("react/addons");
-var Util = require("../helpers/Util");
+var Util = require("../../helpers/Util");
 
-var AppsActions = require("../actions/AppsActions");
-var AppsEvents = require("../events/AppsEvents");
-var appScheme = require("../stores/appScheme");
-var AppsStore = require("../stores/AppsStore");
-var appValidator = require("../validators/appValidator");
+var AppsActions = require("../../actions/AppsActions");
+var AppsEvents = require("../../events/AppsEvents");
+var appScheme = require("../../stores/appScheme");
+var AppsStore = require("../../stores/AppsStore");
+var appValidator = require("../../validators/appValidator");
 var CollapsiblePanelComponent =
-  require("../components/CollapsiblePanelComponent");
+  require("../../components/CollapsiblePanelComponent");
 var ContainerSettingsComponent =
-  require("../components/ContainerSettingsComponent");
-var FormGroupComponent = require("../components/FormGroupComponent");
-var ModalComponent = require("../components/ModalComponent");
+  require("../../components/ContainerSettingsComponent");
+var FormGroupComponent = require("../../components/FormGroupComponent");
+var ModalComponent = require("../../components/ModalComponent");
 var OptionalSettingsComponent =
-  require("../components/OptionalSettingsComponent");
-var ValidationError = require("../validators/ValidationError");
+  require("../../components/OptionalSettingsComponent");
+var ValidationError = require("../../validators/ValidationError");
 
 var AppModalComponent = React.createClass({
   displayName: "AppModalComponent",
